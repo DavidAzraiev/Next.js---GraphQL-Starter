02a0b99f20604b7bd57f1e88a62b6deac9de1276
davidazraiev <davidazraiev@gmail.com>
2021-10-02 13:04:51 +0300

    server: add validation

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from 'type-graphql';
+import { Field, ID, ObjectType, Root } from 'type-graphql';
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
 @ObjectType()
@@ -17,7 +17,9 @@ export class User extends BaseEntity {
   lastName: string;
 
   @Field()
-  name: string;
+  name(@Root() parent: User): string {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
 
   @Field()
   @Column('text', { unique: true })
--- server/src/modules/user/Register.ts
+++ server/src/modules/user/Register.ts
@@ -7,26 +7,19 @@ import {
   Resolver,
   Root,
 } from 'type-graphql';
-import * as bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
+import { RegisterInput } from '../register/RegisterInput';
 
-@Resolver(User)
+@Resolver()
 export class RegisterResolver {
   @Query(() => String)
   async Hello() {
     return 'Hello World!';
   }
 
-  @FieldResolver()
-  async name(@Root() parent: User) {
-    return `${parent.firstName} ${parent.lastName}`;
-  }
-
   @Mutation(() => User)
   async register(
-    @Arg('firstName') firstName: string,
-    @Arg('lastName') lastName: string,
-    @Arg('email') email: string,
-    @Arg('password') password: string
+    @Arg('data') { email, firstName, lastName, password }: RegisterInput
   ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
 
